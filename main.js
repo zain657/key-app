e1552a6368723fa38743c1755d5965416830a759
zain <zainhassouneh07@gmail.com>
2024-06-07 15:51:56 +0300

    Modification of user permissions

--- main.js
+++ main.js
@@ -165,3 +165,25 @@ btn.addEventListener('click', function () {
 
 })
 
+
+document.addEventListener('contextmenu', function(e){
+    e.preventDefault();
+})
+
+document.addEventListener('keydown',function(e){
+    if (e.key == 123) {
+        e.preventDefault();
+    }
+    if (e.ctrlKey && e.shiftKey && e.key == 'I') {
+        e.preventDefault();
+    }
+    if (e.ctrlKey && e.shiftKey && e.key == 'C') {
+        e.preventDefault();
+    }
+    if (e.ctrlKey && e.shiftKey && e.key == 'J') {
+        e.preventDefault();
+    }
+    if (e.ctrlKey && e.key == 'U') {
+        e.preventDefault();
+    }
+})
